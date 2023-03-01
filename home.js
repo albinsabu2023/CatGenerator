716de8d6ca721cb9d43ae1a3991054ce72f8bac3
Albin Sabu <126412402+albinsabu2023@users.noreply.github.com>
2023-03-01 22:25:09 +0530

    Delete home.js

--- home.js
+++
@@ -1,7 +0,0 @@
-const catGen = () => {
-  var img = document.createElement("img");
-  var div = document.getElementById("c-2");
-  img.src =
-    "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
-  div.appendChild(img);
-};
